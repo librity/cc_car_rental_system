10b28fe3cb68e715e11204c0b46f50a6d52aebd0
Luis Geniole <luis.geniole@gmail.com>
2020-04-15 20:24:10 -0300

    Finish customer email validation.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
 class Customer < ApplicationRecord
+  before_save { email.downcase! }
+
   validates :name, presence: { message: 'Nome não pode ficar em branco' }
   validates :email, presence: { message: 'Email não pode ficar em branco' },
                     uniqueness: { message: 'Email deve ser único' },
                     format: { with: URI::MailTo::EMAIL_REGEXP,
-                              message: 'Email não é valido' }
+                              message: 'Email não é valido' },
+                    length: { maximum: 255, message: 'Email muito longo' }
   validates :cpf, presence: { message: 'Cpf não pode ficar em branco' },
                   uniqueness: { message: 'Cpf deve ser único' },
                   cpf: { message: 'Cpf não é valido' },
