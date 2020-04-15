899a93191684769dfcbe14285f7b1f139f951e01
Luis Geniole <luis.geniole@gmail.com>
2020-04-15 20:20:30 -0300

    Refactor model tests, all passing but customer email

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,9 @@
 class Customer < ApplicationRecord
   validates :name, presence: { message: 'Nome não pode ficar em branco' }
   validates :email, presence: { message: 'Email não pode ficar em branco' },
-                    uniqueness: { message: 'Email deve ser único' }
+                    uniqueness: { message: 'Email deve ser único' },
+                    format: { with: URI::MailTo::EMAIL_REGEXP,
+                              message: 'Email não é valido' }
   validates :cpf, presence: { message: 'Cpf não pode ficar em branco' },
                   uniqueness: { message: 'Cpf deve ser único' },
                   cpf: { message: 'Cpf não é valido' },
