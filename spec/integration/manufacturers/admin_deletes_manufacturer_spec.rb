d529a85089900721a4b8fe3f08b05a60bfcfc949
Luis Geniole <luis.geniole@gmail.com>
2020-04-15 14:41:02 -0300

    Add edit and delete manufacturer tests.

---
+++ spec/integration/manufacturers/admin_deletes_manufacturer_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'Admin deletes manufacturer' do
+  scenario 'successfully' do
+    Manufacturer.create!(name: 'Fiat')
+
+    visit root_path
+    click_on 'Fabricantes'
+    click_on 'Fiat'
+    click_on 'Apagar fabricante'
+
+    expect(current_path).to eq manufacturers_path
+    expect(page).to have_content('Nenhum fabricante cadastrado')
+  end
+
+  scenario 'and keep anothers' do
+    Manufacturer.create!(name: 'Fiat')
+    Manufacturer.create!(name: 'Honda')
+
+    visit root_path
+    click_on 'Fabricantes'
+    click_on 'Fiat'
+    click_on 'Apagar fabricante'
+
+    expect(current_path).to eq manufacturers_path
+    expect(page).not_to have_content('Fiat')
+    expect(page).to have_content('Honda')
+  end
+end
---
+++ spec/integration/manufacturers/admin_edit_manufacturer_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+feature 'Admin edits manufacturer' do
+  scenario 'successfully' do
+    Manufacturer.create(name: 'Fiat')
+
+    visit root_path
+    click_on 'Fabricantes'
+    click_on 'Fiat'
+    click_on 'Editar'
+    fill_in 'Nome', with: 'Honda'
+    click_on 'Enviar'
+
+    expect(page).to have_content('Honda')
+  end
+
+  scenario 'successfully' do
+    Manufacturer.create(name: 'Fiat')
+
+    visit root_path
+    click_on 'Fabricantes'
+    click_on 'Fiat'
+    click_on 'Editar'
+    fill_in 'Nome', with: ''
+    click_on 'Enviar'
+
+    expect(page).to have_content('Nome não pode ficar em branco')
+  end
+
+  scenario 'successfully' do
+    Manufacturer.create(name: 'Fiat')
+    Manufacturer.create(name: 'Honda')
+
+    visit root_path
+    click_on 'Fabricantes'
+    click_on 'Fiat'
+    click_on 'Editar'
+    fill_in 'Nome', with: 'Honda'
+    click_on 'Enviar'
+
+    expect(page).to have_content('Nome deve ser único')
+  end
+end
