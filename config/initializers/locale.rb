d61da2ecf621aea2cdb089028231115e432942f3
Luis Geniole <luis.geniole@gmail.com>
2020-04-17 20:10:18 -0300

    Implement dt tags in show views.

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -3,8 +3,11 @@
 # Set application timezone
 Rails.application.config.time_zone = 'America/Sao_Paulo'
 
+# Load all locales in config/locales/*
+I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+
 # Permitted locales available for the application
-I18n.available_locales = :'pt-BR'
+I18n.available_locales = %i[en pt-BR]
 
 # Set default locale to something other than :en
 I18n.default_locale = :'pt-BR'
